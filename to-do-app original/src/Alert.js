97f947b98a402da220accabdf3f8bb28b49def01
stephaniee2 <stephaniejlouie@gmail.com>
2018-12-10 15:24:27 -0600

    static forms, submit button

---
+++ to-do-app original/src/Alert.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Alert extends React.Component {
+
+    render(){
+        return(
+            <div>
+                    <input name="alert-time" type="time" /> 
+            </div>
+        )
+    }
+}
---
+++ to-do-app original/src/Task.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+
+
+
+export default class Task extends React.Component {
+
+    state={
+        tasks: []
+    }
+    render(){
+
+        return(
+
+        )
+    }
+}
