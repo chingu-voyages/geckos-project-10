287492478127ab487c31dc5be5ffe60bbdc877af
stephaniee2 <stephaniejlouie@gmail.com>
2019-02-05 19:22:59 -0800

    in process of setting up server

--- to-do-app original/server/server.js
+++ to-do-app original/server/server.js
@@ -1,11 +1,20 @@
 var express = require('express')
 var app = express()
+const path = require('path')
 const port = 3000
 
+// app.use(express.static(path.join(__dirname, 'client')));
 
-app.get('/', (req, res) => res.sendFile('index.html', {root: './public/'}))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'client', 'index.html')));
+
+// app.get('/client/index.html', (req,res) => {
+//     var list = ["item1", "item2", "item3"];
+//     res.json(list);
+//     console.log('Sent list of items');
+// });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 
-// app.use(express.static(path.join(__dirname, 'dist')));
+// res.sendFile(path.join(__dirname+'/client/index.html'))
+
