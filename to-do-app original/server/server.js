0a45a57824f862fe6095038ddd2f4234065d1f3d
stephaniee2 <stephaniejlouie@gmail.com>
2019-02-05 20:02:19 -0800

    in process of setting up server

--- to-do-app original/server/server.js
+++ to-do-app original/server/server.js
@@ -3,9 +3,9 @@ var app = express()
 const path = require('path')
 const port = 3000
 
-// app.use(express.static(path.join(__dirname, 'client')));
+app.use(express.static(path.join(__dirname, '../client')));
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'client', 'index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/./client')));
 
 // app.get('/client/index.html', (req,res) => {
 //     var list = ["item1", "item2", "item3"];
